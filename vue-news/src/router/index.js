902c8fadd8315a5aa8438c0bf1abe094713b1feb
여다영 <duekdud01@naver.com>
2024-04-25 17:43:20 +0900

    toolbar update

--- vue-news/src/router/index.js
+++ vue-news/src/router/index.js
@@ -11,12 +11,16 @@ export const router = new VueRouter({
     //라우터의 정보를 담는다.
     routes: [
         {
+            path: '/',
+            redirect: '/news',
+        },
+        {
             //path: url 주소
             path: '/news',
+            //component: url 주소로 갔을 때 표시될 컴포넌트
             component: NewsView,
         },
         {
-            //component: url 주소로 갔을 때 표시될 컴포넌트
             path: '/ask',
             component: AskView,
         },
