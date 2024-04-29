a9bd24eea331c954fe4323530e9c8344b9720912
여다영 <duekdud01@naver.com>
2024-04-29 17:30:24 +0900

    vue setting

--- vue-news/src/routes/index.js
+++ vue-news/src/routes/index.js
@@ -1,14 +1,18 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import NewsView from '../views/NewsView.vue'
+import NewsView from '../views/NewsView.vue';
 import AskView from '../views/AskView.vue';
 import JobsView from '../views/JobsView.vue';
+import UserView from '../views/UserView.vue';
+import ItemView from '../views/ItemView.vue';
 
 Vue.use(VueRouter);
 
 //라우터에 대한 정보들을 관리하는 VueRouter 객체
 export const router = new VueRouter({
-    //라우터의 정보를 담는다.
+    //해시값 제거
+    mode: 'history',
+    //라우터의 정보를 담는다. 
     routes: [
         {
             path: '/',
@@ -27,6 +31,18 @@ export const router = new VueRouter({
         {
             path: '/jobs',
             component: JobsView,
+        },
+        {
+            path: '/user',
+            component: UserView,
+        },
+        {
+            path: '/user',
+            component: UserView,
+        },
+        {
+            path: '/item',
+            component: ItemView,
         },
     ]
 });
