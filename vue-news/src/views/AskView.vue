447d55377812b2b73eca520c4a655e40d74f5fa5
여다영 <duekdud01@naver.com>
2024-05-02 10:24:42 +0900

    AskView update

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -13,15 +13,12 @@ export default {
       ask: []
     }
   },
+  //created(): 컴포넌트가 생성되자마자 실행되는 로직이 들어감
   created() {
-    var vm = this;
     fetchAskList()
-      .then(function(response) {
-        vm.ask = response.data;
-      })
-      .catch(function(error) {
-        console.log(error);
-      })
+      .then(response => this.ask = response.data)
+      //에러 핸들링에 대한 처리를 catch에서 다 받아줌
+      .catch(error => console.log(error))
   }
 
 }
