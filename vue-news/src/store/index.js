4869b7df5912b206f7412b08113d8812f4582218
여다영 <duekdud01@naver.com>
2024-05-03 16:49:17 +0900

    vuex module update

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList, fetchAskList, fetchJobsList } from '../api/index.js';
+import mutations from './mutations.js';
+import actions from './actions.js';
 
 Vue.use(Vuex);
 
@@ -23,45 +24,6 @@ export const store = new Vuex.Store({
             return state.news;
         }
     },
-    mutations: {
-        SET_NEWS(state, news) {
-            state.news = news;
-        },
-        SET_ASK(state, ask) {
-            state.ask = ask;
-        },
-        SET_JOBS(state, jobs) {
-            state.jobs = jobs;
-        }
-    },
-    actions: {
-        //context: mutations에 접근할 수 있는 인자
-        FETCH_NEWS(context) {
-            fetchNewsList()
-                .then(response => {
-                    context.commit('SET_NEWS', response.data);
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-        },
-        FETCH_ASK({ commit }) {
-            fetchAskList()
-                .then(({ data }) => {
-                    commit('SET_ASK', data);
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-        },
-        FETCH_JOBS(context) {
-            fetchJobsList()
-                .then(response => {
-                    context.commit('SET_JOBS', response.data);
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-        }
-    }
+    mutations,
+    actions,
 });
